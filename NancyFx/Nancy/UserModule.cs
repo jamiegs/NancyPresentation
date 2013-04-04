843419479157a7d397f92321cd745fd1fa5cb07b
Jamie Snell <jgsnell@gmail.com>
2013-04-04 08:36:01 -0500

    Added nancy library and Usermodule

---
+++ NancyFx/Nancy/UserModule.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nancy;
+
+namespace Nancy
+{
+    public class UserModule : NancyModule
+    {
+        public UserModule()
+        {
+            Get["/user"] = param =>
+                {
+                    return new User();
+                }
+        }
+    }
+    public class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
